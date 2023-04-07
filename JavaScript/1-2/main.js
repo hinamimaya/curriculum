24648d0b0e15790ab6d6367eccca13282144c670
hinamimaya <110370730+hinamimaya@users.noreply.github.com>
2023-04-07 16:57:17 +0900

    js課題　変数定数
    変数・定数の作成

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,15 @@
+/*const price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
+
+上記のエラーはsignment to constant variable.
+ これは定数(const)として宣言している名前を変数(let等)として新しい値を代入しようとしている場合に起こるエラー
+ */
+
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
