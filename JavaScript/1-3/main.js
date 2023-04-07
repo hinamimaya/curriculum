446db03b966ed3036407c6e59b94150f88fa33a8
hinamimaya <110370730+hinamimaya@users.noreply.github.com>
2023-04-07 23:00:22 +0900

    js課題　条件分岐
    条件分岐・演算子の作成

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,23 @@
+console.log("問１");
+let a = 10;
+console.log(a != 10);
+
+let b = 14;
+console.log(b<=20 && b>=10 && b%2===0);
+
+console.log("問２");
+let x = 10;
+if(x=>10 && x<=20){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+console.log("問３");
+let y = 0;
+if(y%2 === 0){
+    console.log("偶数です");
+}
+if(y%2 !== 0){
+    console.log("奇数です");
+}
