3a747ea44db5090667496d9b816af11b251d7046
hinamimaya <110370730+hinamimaya@users.noreply.github.com>
2023-04-11 22:41:27 +0900

    js課題　繰り返し
    繰り返しの作成

---
+++ JavaScript/1-4/main.js
@@ -0,0 +1,15 @@
+let number = 1;
+while (number <= 100) {
+    if((number%3)==0){
+        console.log("Fizz!");
+
+    }else if((number%5)==0){
+        console.log("Buzz!");
+    }else if((number%3==0)&&(number%5==0)){
+        console.log("FizzBuzz!!");
+    }
+
+    else{
+        console.log(number);
+    }
+}
