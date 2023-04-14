2075e68b285d1082f6b340577d99ff514bc5a03d
hinamimaya <110370730+hinamimaya@users.noreply.github.com>
2023-04-14 19:26:32 +0900

    js課題　1章チェックテスト
    1章チェックテストの作成

---
+++ JavaScript/Check1/main.js
@@ -0,0 +1,13 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+function isEven() {
+    for(let i =0; i < numbers.length; i++){
+            if(numbers[i]%2==0){
+                num = numbers[i];   /*numに、偶数のiが蓄積されていってほしい*/  
+            }else {
+                ;
+            }
+        } 
+        console.log(num + 'は偶数です');
+}
+
